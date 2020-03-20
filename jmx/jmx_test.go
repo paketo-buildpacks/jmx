fcebfaffc5e330a67c9959fe55fb261c66de0ad9
Ben Hale <bhale@pivotal.io>
2020-03-20 08:20:52 -0700

    Polishing
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- jmx/jmx_test.go
+++ jmx/jmx_test.go
@@ -62,6 +62,7 @@ export JAVA_OPTS="${JAVA_OPTS}
   -Dcom.sun.management.jmxremote.authenticate=false
   -Dcom.sun.management.jmxremote.ssl=false
   -Dcom.sun.management.jmxremote.port=${PORT}
-  -Dcom.sun.management.jmxremote.rmi.port=${PORT}"`))
+  -Dcom.sun.management.jmxremote.rmi.port=${PORT}"
+`))
 	})
 }
